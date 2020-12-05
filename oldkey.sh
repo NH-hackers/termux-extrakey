7e3a9a28842cac0f5551377191ab8435beb7f544
NH hackers <74050570+NH-hackers@users.noreply.github.com>
2020-12-05 12:55:23 +0530

    Add files via upload

---
+++ oldkey.sh
@@ -0,0 +1,31 @@
+printf " \e[1;32m
+
+ ██████╗ ██╗     ██████╗         
+██╔═══██╗██║     ██╔══██╗        
+██║   ██║██║     ██║  ██║        
+██║   ██║██║     ██║  ██║        
+╚██████╔╝███████╗██████╔╝        
+ ╚═════╝ ╚══════╝╚═════╝         
+                                 
+██╗  ██╗███████╗██╗   ██╗███████╗
+██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔════╝
+█████╔╝ █████╗   ╚████╔╝ ███████╗
+██╔═██╗ ██╔══╝    ╚██╔╝  ╚════██║
+██║  ██╗███████╗   ██║   ███████║
+╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝
+                                 
+\e[0m\n"
+read -p $' \e[1;32m
+    \e[31m❰\e[0m❰\e[32m❰  press enter to contune..\e[31m❱\e[0m❱\e[32m❱ \e[0m\e[1;32m\en'
+echo "";
+~/.oldkey
+echo -e "\e[1;31m ☯\e[32mgeting keys..☞\e[0m";
+echo "";
+rm ~/.termux/termux.properties &> /dev/null;
+
+echo -e "\e[1;32m ☯\e[32mset up settings.☞\e[0m";
+echo "";
+mv termux.properties ~/.termux &> /dev/null;
+sleep 2
+termux-reload-settings
+exit 0
